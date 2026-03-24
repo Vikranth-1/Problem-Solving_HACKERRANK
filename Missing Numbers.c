f5112bc1569f2a799b94f59997666aa7a9b72d6a
Vikranth V <vvikranth08@gmail.com>
2026-03-24 19:19:55 +0530

    Create Missing Numbers.c

---
+++ Missing Numbers.c
@@ -0,0 +1,223 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+int* missingNumbers(int arr_count, int* arr, int brr_count, int* brr, int* result_count) {
+
+    // Find min and max in brr
+    int min = INT_MAX, max = INT_MIN;
+    for (int i = 0; i < brr_count; i++) {
+        if (brr[i] < min) min = brr[i];
+        if (brr[i] > max) max = brr[i];
+    }
+
+    int range = max - min + 1;
+
+    // Frequency arrays
+    int* freqA = calloc(range, sizeof(int));
+    int* freqB = calloc(range, sizeof(int));
+
+    // Count frequencies
+    for (int i = 0; i < arr_count; i++) {
+        freqA[arr[i] - min]++;
+    }
+
+    for (int i = 0; i < brr_count; i++) {
+        freqB[brr[i] - min]++;
+    }
+
+    // Result array (max possible size = range)
+    int* result = malloc(range * sizeof(int));
+    int k = 0;
+
+    for (int i = 0; i < range; i++) {
+        if (freqB[i] > freqA[i]) {
+            result[k++] = i + min;
+        }
+    }
+
+    *result_count = k;
+
+    free(freqA);
+    free(freqB);
+
+    return result;
+}
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    int m = parse_int(ltrim(rtrim(readline())));
+
+    char** brr_temp = split_string(rtrim(readline()));
+
+    int* brr = malloc(m * sizeof(int));
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = parse_int(*(brr_temp + i));
+
+        *(brr + i) = brr_item;
+    }
+
+    int result_count;
+    int* result = missingNumbers(n, arr, m, brr, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, " ");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
