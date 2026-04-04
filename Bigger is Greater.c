33bc1c2061fd02742ec2446961cbbaf6a1e5a237
Vikranth V <vvikranth08@gmail.com>
2026-04-04 15:34:50 +0530

    Create Bigger is Greater.c

---
+++ Bigger is Greater.c
@@ -0,0 +1,172 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+char* biggerIsGreater(char* w) {
+    int n = strlen(w);
+    char* result = malloc((n + 1) * sizeof(char));
+    strcpy(result, w);
+    
+    int i = n - 2;
+    while (i >= 0 && result[i] >= result[i + 1]) {
+        i--;
+    }
+    
+    if (i < 0) {
+        free(result);
+        char* no_answer = malloc(11 * sizeof(char));
+        strcpy(no_answer, "no answer");
+        return no_answer;
+    }
+    
+    int j = n - 1;
+    while (result[j] <= result[i]) {
+        j--;
+    }
+    
+    char temp = result[i];
+    result[i] = result[j];
+    result[j] = temp;
+    
+    int left = i + 1;
+    int right = n - 1;
+    while (left < right) {
+        char t = result[left];
+        result[left] = result[right];
+        result[right] = t;
+        left++;
+        right--;
+    }
+    
+    return result;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int T = parse_int(ltrim(rtrim(readline())));
+
+    for (int T_itr = 0; T_itr < T; T_itr++) {
+        char* w = readline();
+
+        char* result = biggerIsGreater(w);
+
+        fprintf(fptr, "%s\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
