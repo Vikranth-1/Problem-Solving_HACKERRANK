82ae52a003e25e8fdde85713e4b7e4740505c994
Vikranth V <vvikranth08@gmail.com>
2026-04-12 12:11:54 +0530

    Create Weighted Uniform Strings.c

---
+++ Weighted Uniform Strings.c
@@ -0,0 +1,182 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+char** weightedUniformStrings(char* s, int queries_count, int* queries, int* result_count) {
+    *result_count = queries_count;
+    char** result = malloc(queries_count * sizeof(char*));
+    
+    int* weights = calloc(10000000, sizeof(int));
+    int len = strlen(s);
+    
+    for (int i = 0; i < len; i++) {
+        int weight = s[i] - 'a' + 1;
+        int count = 1;
+        weights[weight] = 1;
+        
+        while (i + 1 < len && s[i] == s[i + 1]) {
+            count++;
+            weights[weight * count] = 1;
+            i++;
+        }
+    }
+    
+    for (int i = 0; i < queries_count; i++) {
+        if (queries[i] <= 10000000 && weights[queries[i]]) {
+            result[i] = malloc(4 * sizeof(char));
+            strcpy(result[i], "Yes");
+        } else {
+            result[i] = malloc(3 * sizeof(char));
+            strcpy(result[i], "No");
+        }
+    }
+    
+    free(weights);
+    return result;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    int queries_count = parse_int(ltrim(rtrim(readline())));
+
+    int* queries = malloc(queries_count * sizeof(int));
+
+    for (int i = 0; i < queries_count; i++) {
+        int queries_item = parse_int(ltrim(rtrim(readline())));
+
+        *(queries + i) = queries_item;
+    }
+
+    int result_count;
+    char** result = weightedUniformStrings(s, queries_count, queries, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%s", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
