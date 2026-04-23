d54460dae16836daf8d9a8edd7b43248267a6e78
Vikranth V <vvikranth08@gmail.com>
2026-04-23 16:05:47 +0530

    Create Making Anagrams.c

---
+++ Making Anagrams.c
@@ -0,0 +1,100 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+int makingAnagrams(char* s1, char* s2) {
+    int count[26] = {0};
+    
+    for (int i = 0; s1[i] != '\0'; i++) {
+        count[s1[i] - 'a']++;
+    }
+    
+    for (int i = 0; s2[i] != '\0'; i++) {
+        count[s2[i] - 'a']--;
+    }
+    
+    int deletions = 0;
+    for (int i = 0; i < 26; i++) {
+        deletions += abs(count[i]);
+    }
+    
+    return deletions;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s1 = readline();
+
+    char* s2 = readline();
+
+    int result = makingAnagrams(s1, s2);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
